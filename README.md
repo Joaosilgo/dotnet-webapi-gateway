1470e4751767001d59c66597bd34ebff72c47064
João Gomes <joaosilgo96@gmail.com>
2021-07-02 00:27:25 +0100

    🌮

---
+++ README.md
@@ -0,0 +1,24 @@
+﻿# API GATEWAY 🌮
+
+![Gateway](https://images.unsplash.com/photo-1602465579787-49cc7ab66f6c?ixid=MnwxOTkyMTB8MHwxfGNvbGxlY3Rpb258MXwzY1hKR09YdkpZOHx8fHx8Mnx8MTYyNTE4MDk1Nw&ixlib=rb-1.2.1)
+
+## OCELOT
+
+![Ocelot](https://images.unsplash.com/photo-1607374072031-e3333f4a7c7a?ixid=MnwxOTkyMTB8MHwxfGNvbGxlY3Rpb258MXwzY1hKR09YdkpZOHx8fHx8Mnx8MTYyNTE4MTIwNg&ixlib=rb-1.2.1)
+
+Ocelot is a .NET API Gateway. This project is aimed at people using .NET running a micro services / service oriented architecture that need a unified point of entry into their system. However it will work with anything that speaks HTTP and run on any platform that ASP.NET Core supports.
+
+In particular I want easy integration with bearer tokens.
+
+## RATE LIMIT
+
+![Ocelot](https://images.unsplash.com/photo-1614028674026-a65e31bfd27c?ixid=MnwxOTkyMTB8MHwxfGNvbGxlY3Rpb258MXwzY1hKR09YdkpZOHx8fHx8Mnx8MTYyNTE4MTQzNw&ixlib=rb-1.2.1)
+
+````bash
+git init
+git add .
+git commit -m "🌮"
+git branch -M main
+git remote add origin https://github.com/Joaosilgo/dotnet-webapi-gateway.git
+git push -u origin main
+````
---
+++ Startup.cs
@@ -0,0 +1,117 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetWebApiGateway
+{
+    public class Startup
+    {
+
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+
+
+            var secret = "Thisismytestprivatekey";
+            var key = Encoding.ASCII.GetBytes(secret);
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+
+            services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
+            // services.AddSwaggerForOcelot(Configuration);
+            services.AddSwaggerForOcelot(Configuration,
+            (o) =>
+            {
+                o.GenerateDocsForGatewayItSelf = true;
+            });
+
+
+
+            /*
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+            */
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.UseSwagger();
+            app.UseSwaggerForOcelotUI();
+
+            app.UseRouting();
+            app.UseAuthentication();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Welcome To Api Gateway of !!");
+                });
+            });
+            // app.UseSwagger().UseOcelot().Wait();
+            // app.UseSwagger();
+
+            /*
+            app.UseSwaggerForOcelotUI(opt =>
+            {
+                //opt.PathToSwaggerGenerator = "/swagger/docs";
+                //opt.
+            })
+            .UseOcelot().Wait();
+            
+
+            app.UseSwagger();
+            */
+
+            app.UseOcelot()
+                .Wait();
+
+        }
+    }
+}
